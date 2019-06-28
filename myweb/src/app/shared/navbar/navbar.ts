c6a40705953abbec254b099350ec2a07fb6ab0ab
dharmeshpipariya <pipariyadharmesh@gmail.com>
2019-06-28 11:21:57 +0530

    design update

--- myweb/src/app/shared/navbar/navbar.ts
+++ myweb/src/app/shared/navbar/navbar.ts
@@ -1,12 +1,19 @@
-import { Component, NgModule } from '@angular/core';
+import { Component, NgModule, ViewEncapsulation } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.html',
-  styleUrls: ['./navbar.scss']
+  styleUrls: ['./navbar.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class NavBar { }
+export class NavBar {
+  isNavExpanded: boolean = false;
+  toggleNav() {
+    this.isNavExpanded = !this.isNavExpanded;
+    document.body.classList.toggle('navbar-expanded');
+  }
+}
 
 @NgModule({
   imports: [
